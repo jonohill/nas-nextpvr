f0c13d537868895fc45aab41acf1bfb1b4b11302
Jono Hill <jono@hillnz.com>
2023-01-03 21:39:37 +1300

    Try to detect movies

--- scripts/PostProcessing.sh
+++ scripts/PostProcessing.sh
@@ -18,6 +18,11 @@ from xml.etree import ElementTree
 import magic
 
 
+BLACKLIST = [
+    'infomercial',
+]
+
+
 def env(var_name):
     """Get env var by name"""
     try:
@@ -59,6 +64,58 @@ def delete_stale_recordings(dir: str):
         os.remove(f)
 
 
+def is_blacklisted(title: str) -> bool:
+    """Check if title is blacklisted"""
+    for word in BLACKLIST:
+        if word in title.lower():
+            return True
+    return False
+
+
+def is_movie(metadata: dict[str, str]):
+    if 'movie' in metadata.get('genre', '').lower():
+        return True
+    return False
+
+
+def copy_with_metadata(filename: str, out_dir: str, metadata: dict[str, str], artwork: str | None):
+
+    name, _ = os.path.splitext(args.filename)
+
+    with TemporaryDirectory() as tmpdir:
+
+        metadata_args: list[str] = []
+        for k, v in metadata.items():
+            metadata_args += ['-metadata', f'{k}={v}']        
+
+        artwork_args = []
+        if artwork:
+            try:
+                print('Downloading ' + artwork)
+                artwork_file, _ = urlretrieve(artwork)
+
+                IMAGES = {
+                    'image/jpeg': 'jpg',
+                    'image/png': 'png'
+                }
+
+                mimetype: str = magic.from_file(artwork_file, mime=True)
+                if image_ext := IMAGES.get(mimetype):
+                    cover = os.path.join(tmpdir, 'cover.' + image_ext)
+                    copyfile(artwork_file, cover)
+                    artwork_args = ['-i', cover, '-map', '1', '-map', '0', '-disposition:0', 'attached_pic']
+            except Exception as err:
+                print(f"Error downloading artwork: {err}")
+
+        output_file = os.path.join(out_dir, os.path.basename(name) + '.mp4')
+        ff_args = ['ffmpeg', '-i', filename] \
+            + artwork_args + metadata_args \
+            + ['-dn', '-acodec', 'copy', '-vcodec', 'copy', '-y', output_file]
+        print(' '.join(shlex.quote(arg) for arg in ff_args))
+
+        run(ff_args, check=True)
+
+
 class Args(argparse.Namespace):
     """Args"""
     filename: str
@@ -76,8 +133,6 @@ print(f"Post processing {args.filename}...")
 
 RECORDED_DIR = env('RECORDED_DIR')
 NEXTPVR_DIR = env('NEXTPVR_DATADIR_USERDATA').rstrip('/')
-
-name, ext = os.path.splitext(args.filename)
 
 conn = sqlite3.connect(os.path.join(NEXTPVR_DIR, 'npvr.db3'))
 
@@ -102,39 +157,13 @@ for item in event_xml:
             metadata['episode_id'] = text
         if item.tag == 'DeferredArtwork':
             artwork = text
+    elif item.tag == 'Genres':
+        metadata['genre'] = ', '.join(genre.text for genre in item if genre.text)
 
-metadata_args: list[str] = []
-for k, v in metadata.items():
-    metadata_args += ['-metadata', f'{k}={v}']
 
-with TemporaryDirectory() as tmpdir:
-
-    artwork_args = []
-    if artwork:
-        try:
-            print('Downloading ' + artwork)
-            artwork_file, _ = urlretrieve(artwork)
-
-            IMAGES = {
-                'image/jpeg': 'jpg',
-                'image/png': 'png'
-            }
-
-            mimetype: str = magic.from_file(artwork_file, mime=True)
-            if image_ext := IMAGES.get(mimetype):
-                cover = os.path.join(tmpdir, 'cover.' + image_ext)
-                copyfile(artwork_file, cover)
-                artwork_args = ['-i', cover, '-map', '1', '-map', '0', '-disposition:0', 'attached_pic']
-        except Exception as err:
-            print(f"Error downloading artwork: {err}")
-
-    output_file = os.path.join(RECORDED_DIR, os.path.basename(name) + '.mp4')
-    ff_args = ['ffmpeg', '-i', args.filename] \
-        + artwork_args + metadata_args \
-        + ['-dn', '-acodec', 'copy', '-vcodec', 'copy', '-y', output_file]
-    print(' '.join(shlex.quote(arg) for arg in ff_args))
-
-    run(ff_args, check=True)
+if not is_blacklisted(args.filename):
+    out_dir = os.path.join(RECORDED_DIR, 'Movies' if is_movie(metadata) else 'TV Shows')
+    copy_with_metadata(args.filename, out_dir, metadata, artwork)
 
 os.remove(args.filename)
 # If there are no other video files in its directory, delete
